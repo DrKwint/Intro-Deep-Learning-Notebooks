24881380cb5c08ed137a95799453fe05a8e57d19
Eleanor Quint <equint@cse.unl.edu>
2021-01-20 23:22:04 -0600

    Update create_env.sh

--- 2020S_hackathons/create_env.sh
+++ 2020S_hackathons/create_env.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+### Creates a Conda environment called `tensorflow-env` with GPU-capable TensorFlow 2 installed
 
+# Clone a GPU-enabled environment
 module load tensorflow-gpu/py38/2.3
 module load anaconda
 conda create -n tensorflow-env --clone $CONDA_DEFAULT_ENV
 module unload tensorflow-gpu/py38/2.3
 
+# Activate the new environment
 conda activate tensorflow-env
+
+# Setup as Jupyter environment
+python -m ipykernel install --user --name "$CONDA_DEFAULT_ENV" --display-name "Python ($CONDA_DEFAULT_ENV)"
+mkdir -p $WORK/.jupyter
+mv ~/.local/share/jupyter/kernels $WORK/.jupyter
+ln -s $WORK/.jupyter/kernels ~/.local/share/jupyter/kernels
