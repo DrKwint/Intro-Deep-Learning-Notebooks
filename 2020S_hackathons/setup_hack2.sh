63539b51641ae6c7867df652a40ca5a358731182
Eleanor Quint <equint@cse.unl.edu>
2021-01-20 23:18:33 -0600

    Update setup_hack2.sh

--- 2020S_hackathons/setup_hack2.sh
+++ 2020S_hackathons/setup_hack2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Add a symlink from $HOME, which is read only from worker nodes, to $WORK which is not
+mkdir $WORK/tensorflow_datasets
+ln -s $WORK/tensorflow_datasets $HOME/tensorflow_datasets
+
+# Install python dependencies
 conda activate tensorflow-env
 pip install numpy tensorflow tqdm tensorflow_datasets matplotlib
