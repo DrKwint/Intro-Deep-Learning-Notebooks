bafa8cd94aa6c3550c7b8da09a47795dabf8b750
Quint <DrKwint@gmail.com>
2022-01-12 22:58:37 -0600

    Ensure everything is working on Crane

--- 2020S_hackathons/create_env.sh
+++ 2020S_hackathons/create_env.sh
@@ -2,13 +2,14 @@
 ### Creates a Conda environment called `tensorflow-env` with GPU-capable TensorFlow 2 installed
 
 # Clone a GPU-enabled environment
-module load tensorflow-gpu/py38/2.3
+module load tensorflow-gpu/py39/2.6
 module load anaconda
 conda create -n tensorflow-env --clone $CONDA_DEFAULT_ENV
-module unload tensorflow-gpu/py38/2.3
+module unload tensorflow-gpu/py39/2.6
 
 # Activate the new environment
 conda activate tensorflow-env
+pip install tensorflow-datasets tqdm matplotlib ipywidgets gym scipy tensorflow_hub
 
 # Setup as Jupyter environment
 python -m ipykernel install --user --name "$CONDA_DEFAULT_ENV" --display-name "Python ($CONDA_DEFAULT_ENV)"
--- 2020S_hackathons/setup_hack3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load anaconda
-conda activate tensorflow-env
-pip install numpy tensorflow tqdm tensorflow_datasets matplotlib
